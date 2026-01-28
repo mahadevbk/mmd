ad1af6c71e48f32babfac26da616eff2d53bdace
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-01-28 16:03:55 +0400

    Update mmd.py

--- mmd.py
+++ mmd.py
@@ -394,7 +394,17 @@ if 'image_urls' not in st.session_state:
 
 
 
-# --- Functions ---
+# --- Session state initialization ---
+if 'players_df' not in st.session_state:
+    st.session_state.players_df = pd.DataFrame(columns=["name", "profile_image_url", "birthday"])
+if 'matches_df' not in st.session_state:
+    st.session_state.matches_df = pd.DataFrame(columns=["match_id", "date", "match_type", "team1_player1", "team1_player2", "team2_player1", "team2_player2", "set1", "set2", "set3", "winner", "match_image_url"])
+
+#-----functions ------
+load_players()
+load_matches()
+
+
 
 @st.cache_data(ttl=600)  # Add this line (caches for 10 minutes)
 def load_matches():
