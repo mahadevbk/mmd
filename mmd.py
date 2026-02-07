c389257c6bbcd0ecc70c4616da5e6625e9702f52
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-02-07 12:25:50 +0400

    Update mmd.py

--- mmd.py
+++ mmd.py
@@ -2962,6 +2962,7 @@ with tabs[0]:
 
     
     st.header(f"Rankings as of {datetime.now().strftime('%d %b %Y')}")
+    st.info("MMD Points award : 3 for a Mixed Doubles win, 2 for a Doubles game win, 2 for a Singles game win, 1 for a loss(of any kind of game) & 1.5 for a Tie in any kind of game.")
     ranking_type = st.radio(
         "Select Ranking View",
         ["Combined", "Doubles", "Singles", "Nerd Stuff", "Table View"],
