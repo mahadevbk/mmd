ebbd96beda22c1cd64521beb01d25a3c6a1d5a6d
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-03-06 16:28:16 +0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,17 @@
+const CACHE_NAME = 'mmd-v3';
+const ASSETS = [
+  '/',
+  'https://raw.githubusercontent.com/mahadevbk/mmd/main/assets/logo/mmdlogo.png'
+];
+
 self.addEventListener('install', (e) => {
-  console.log('MMD Service Worker Installed');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
