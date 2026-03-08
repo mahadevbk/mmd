b6a612bc4708c6f6e5c16b3e85a425403e5b22c6
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-03-08 15:15:32 +0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,23 +1,19 @@
-const CACHE_NAME = 'mmd-v14';
+const CACHE_NAME = 'mmd-v25';
 const ASSETS = [
-  '/',
-  '/static/manifest.json',
-  '/static/mmdlogo-192.png',
-  '/static/mmdlogo-512.png'
+  'https://mmdtennis.streamlit.app/',
+  'https://raw.githubusercontent.com/mahadevbk/mmd/main/static/manifest.json',
+  'https://raw.githubusercontent.com/mahadevbk/mmd/main/static/mmdlogo-192.png',
+  'https://raw.githubusercontent.com/mahadevbk/mmd/main/static/mmdlogo-512.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
