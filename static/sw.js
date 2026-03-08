0101964ca3bdaaf9999c352603d6eb270a221804
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-03-08 14:48:17 +0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,20 +1,17 @@
-const CACHE_NAME = 'mmd-v12'; // Incremented version
+const CACHE_NAME = 'mmd-v12';
 const ASSETS = [
   '/',
-  './manifest.json',        // Relative to the sw.js location
+  './manifest.json',
   './mmdlogo-192.png',
   './mmdlogo-512.png'
 ];
 
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[Service Worker] Caching all assets');
-      // Using map to catch individual errors so one failure doesn't kill the whole SW
       return Promise.all(
         ASSETS.map(url => {
-          return cache.add(url).catch(err => console.error('Failed to cache:', url, err));
+          return cache.add(url).catch(err => console.log('Asset not found:', url));
         })
       );
     })
@@ -22,8 +19,6 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  console.log('[Service Worker] Activate');
-  // Clean up old caches
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(keys.map((key) => {
