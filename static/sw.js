cfbfe8a89b154f9165be17f4d451651d5165cb2a
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-03-08 15:01:14 +0400

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,36 +1,23 @@
-const CACHE_NAME = 'mmd-v13';
+const CACHE_NAME = 'mmd-v14';
 const ASSETS = [
   '/',
-  './manifest.json',
-  './mmdlogo-192.png',
-  './mmdlogo-512.png'
+  '/static/manifest.json',
+  '/static/mmdlogo-192.png',
+  '/static/mmdlogo-512.png'
 ];
-
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return Promise.all(
-        ASSETS.map(url => {
-          return cache.add(url).catch(err => console.log('Asset not found:', url));
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
+      return cache.addAll(ASSETS);
     })
   );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
