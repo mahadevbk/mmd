353ea53ffe9d6858244780d3106d38769a56cda7
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2026-03-25 17:56:29 +0400

    Update mmd.py

--- mmd.py
+++ mmd.py
@@ -144,7 +144,7 @@ div[data-testid="stExpander"] {
     padding: 8px 12px;
     border-radius: 20px;
     backdrop-filter: blur(5px);
-    border: 1px solid rgba(255, 255, 255, 0.1);
+    border: 5px solid rgba(255, 255, 255, 0.1);
 }
 
 /* Individual Icon Style */
